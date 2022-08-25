fa1793e713479d24b12837bd6d09dfb2d2ba99ab
Matias Aguirre <aguirremati09@gmail.com>
2022-08-25 16:48:44 -0300

    API applied

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 import './style.css';
+import dynamicScore from './modules/scores.js';
+
+const originalURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
+const id = '49UE7WNSjc9vGKaTdRhI';
+const refreshButton = document.getElementById('refresh');
+const user = document.getElementById('user');
+const score = document.getElementById('score');
+const submitButton = document.getElementById('submit');
+const scoreList = document.getElementById('board');
+
+const sendData = async () => {
+  const response = await fetch(`${originalURL}/games/${id}/scores/`, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: user.value,
+      score: score.value,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const success = await response.json();
+  return success;
+};
+submitButton.addEventListener('click', sendData);
+
+const receiveData = async () => {
+  const response = await fetch(`${originalURL}/games/${id}/scores/`, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const gamerArray = await response.json();
+  const actualArray = gamerArray.result;
+  scoreList.innerHTML = '';
+  actualArray.forEach((item) => { dynamicScore(item.user, item.score); });
+  user.value = null;
+  score.value = null;
+};
+refreshButton.addEventListener('click', receiveData);
