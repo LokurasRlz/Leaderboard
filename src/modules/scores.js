78de0ef210035d2a24a6abb2f99d9cc6291412aa
Matias Aguirre <aguirremati09@gmail.com>
2022-08-25 17:15:03 -0300

    Fixed Score Arrow Function.

--- src/modules/scores.js
+++ src/modules/scores.js
@@ -1,7 +1,9 @@
-export default function dynamicScore(userValue, scoreValue) {
+const dynamicScore = (userValue, scoreValue) => {
   const scoreList = document.getElementById('board');
   const scoreItem = document.createElement('li');
   scoreItem.classList.add('li');
   scoreItem.innerHTML += `${userValue}: ${scoreValue}`;
   scoreList.appendChild(scoreItem);
 }
+
+export default dynamicScore;
